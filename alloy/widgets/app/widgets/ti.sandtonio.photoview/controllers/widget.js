c3a309f652db21c3a6058b33fe208133f54210e5
Antonio <berser.ar@gmail.com>
2013-12-13 16:16:22 -0600

    rm git

---
+++ alloy/widgets/app/widgets/ti.sandtonio.photoview/controllers/widget.js
@@ -0,0 +1,28 @@
+var takePhoto =require('ti.sandtonio.photoview/photo').showCamera;
+
+var numberPhotos = 0;
+
+var photos = {};
+
+$.listPhotos.addEventListener('delete_view', function(e){
+	$.listPhotos.remove(photos[e.id].getView());
+	delete photos[e.id];
+});
+
+function newPhoto(){
+	takePhoto({
+		success:function(image){
+			photos[numberPhotos] = Alloy.createWidget('ti.sandtonio.photoview','view', {id:numberPhotos, image:image});			
+			$.listPhotos.add(photos[numberPhotos].getView());	
+			numberPhotos++;	
+		}
+	});	
+}
+
+function uploadPhotos(){
+	_.each(_.keys(photos),
+		function(photo){
+			Ti.API.info(photos[photo].image());
+		}
+	);
+}
